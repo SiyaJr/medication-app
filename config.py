9763da0ae11dae809932130fd02acbcd26b9d2e8
Siyabonga <siyabongamathonsi7@gmail.com>
2025-05-17 14:28:29 +0200

    Update everything

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
-DB_HOST = "localhost"
-DB_USER = "med_user"
-DB_PASSWORD = "Siya123"
-DB_NAME = "medication_app"
-SECRET_KEY = "Siya123"
-
+DB_HOST = "postgresql://siya123:jzqf9Y82w4SHcNxvw7iuaA7VG9coodK6@dpg-d0k7kbt6ubrc73b1s2m0-a.oregon-postgres.render.com/medicationdb_vknv"
+DB_USER = "siya123"
+DB_PASSWORD = "jzqf9Y82w4SHcNxvw7iuaA7VG9coodK6"
+DB_NAME = "medicationdb_vknv"
+SECRET_KEY = "siya123"
+DB_PORT ="5432"
